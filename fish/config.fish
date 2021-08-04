b2ec4d7c99a94e75628282df040a006ca00192be
Paul Kim <44695374+thekpaul@users.noreply.github.com>
2021-08-04 21:45:58 +0900

    Add Configurations for PyEnv

--- fish/config.fish
+++ fish/config.fish
@@ -2,3 +2,5 @@ set -Ux fish_greeting "Welcome! Let's get Started..."
 cd
 rvm default
 set -gx PATH /usr/local/texlive/2021/bin/x86_64-linux $PATH
+status is-interactive; and pyenv init --path | source
+pyenv init - | source
