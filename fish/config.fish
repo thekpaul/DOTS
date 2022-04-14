dae257221a2ab7aae436cd6fa22275c73dc9b9a2
Paul Kim <44695374+thekpaul@users.noreply.github.com>
2022-04-14 21:31:25 +0900

    Prepend System-wide TexLive 2022 Directory to $PATH

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,7 @@
 set -Ux fish_greeting "Welcome! Let's get Started..."
 cd
 rvm default
-set -gx PATH /usr/local/texlive/2021/bin/x86_64-linux $PATH
+set -gx PATH /usr/local/texlive/2022/bin/x86_64-linux \
+             /usr/local/texlive/2021/bin/x86_64-linux $PATH
 status is-interactive; and pyenv init --path | source
 pyenv init - | source
