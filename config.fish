a3d4e5587db5793b2560b08df47d5d1f7275a9fb
Paul Kim <44695374+thekpaul@users.noreply.github.com>
2022-05-13 07:25:22 +0900

    Add Language Settings for Perl with NeoVim

--- config.fish
+++ config.fish
@@ -5,3 +5,5 @@ set -gx PATH /usr/local/texlive/2022/bin/x86_64-linux \
              /usr/local/texlive/2021/bin/x86_64-linux $PATH
 status is-interactive; and pyenv init --path | source
 pyenv init - | source
+set -gx LC_CTYPE en_US.UTF-8
+set -gx LC_ALL en_US.UTF-8
