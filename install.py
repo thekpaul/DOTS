3c8b02d0b985b14103e13833c45166d6fcb1797d
Paul Kim <44695374+thekpaul@users.noreply.github.com>
2022-07-08 05:11:22 +0900

    Add Basic Structure for Main Page

--- install.py
+++ install.py
@@ -12,9 +12,70 @@ import sys
 import argparse
 
 if __name__ == "__main__": # MAIN Function
+
+  # CHECK if Python is above 3.10 => Necessary for `match-case`
+
+  # Supported Collections: FISH | GIT | NVIM | TEX
+
+  # Supported Pacakges:
+  #   FISH: fish, omf
+  #   GIT : git,  gh
+  #   NVIM: nvim
+  #   TEX : (tex installation from internet)
+
   while True: # Start with Printing Main Page
+
+          #1234567890123456789012345678901234567890123456789012345678901234#
+    print("###                DOTS INTERACTIVE INSTALLER                ###")
+    print("                                                                ")
+    print("     This is the DOTS Interactive Installer, a tool for         ")
+    print("     installing and configuring my dotfiles interactively.      ")
+    print("                                                                ")
+    print("  [C] Collections: x out of y specified                         ")    # Collections
+    print("                                                                ")
+    print("                                                                ")
+    print("  [P] Packages: x out of y specified                            ")    # Packages
+    print("                                                                ")
+    print("                                                                ")
+    print("  [D] Directories                                               ")    # Directories
+    print("                                                                ")
+    print("      $HOME:                                                    ")    # $HOME
+    print("      SYMLINKS: x out of y specified                            ")    # `x/n symlinks specified`
+    print("                                                                ")
+    print("  [O] Options                                                   ")    # Options
+    print("                                                                ")
+    print("      ( ) Set FISH as Default Shell                             ")
+    print("                                                                ")
+    print("  Actions                                                       ")    # Interactions
+    print("                                                                ")
+    print("      [I] Install DOTS                                          ")    # INSTALL
+    print("      [S] Save Current Settings to $HOME and Exit               ")    # SAVE
+    print("      [Q] Quit Installer                                        ")    # QUIT
+    print("                                                                ")
+
     match input_char:
       case 'c' | 'C': # Collections
+        while True:   # Start with Printing Collections Page
+
       case 'p' | 'P': # Packages
+        while True:   # Start with Printing Packages Page
+
+      case 'd' | 'D': # Directories
+        while True:   # Start with Printing Directories Page
+
       case 'o' | 'O': # Options
+        while True:   # Start with Printing Options Page
+
+      case 'i' | 'I': # Begin Installation
+        # Commence System-Level Installation
+        break
+
+      case 's' | 'S': # Save Current Installer Profile to HOME DIR
+        # Save with JSON file
+        break
+
+      case 'q' | 'Q': # Exit
+        break
+
       case _: # Unrecognisable => continue
+        continue
