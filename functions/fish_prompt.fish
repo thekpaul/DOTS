db432f14835008627a1a48f0514a96a3aa64709a
Paul Kim <44695374+thekpaul@users.noreply.github.com>
2022-11-02 19:33:03 +0900

    Append Git Branch to Fish Prompt

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -1,29 +1,36 @@
 function fish_prompt
-    if not set -q VIRTUAL_ENV_DISABLE_PROMPT
-        set -g VIRTUAL_ENV_DISABLE_PROMPT true
-    end
+  if not set -q VIRTUAL_ENV_DISABLE_PROMPT
+    set -g VIRTUAL_ENV_DISABLE_PROMPT true
+  end
 
-    echo
+  echo
 
-    set_color yellow
-    printf '%s' $USER
-    set_color normal
-    printf ' at '
+  set_color yellow
+  printf '%s' $USER
+  set_color normal
+  printf ' at '
 
-    set_color magenta
-    echo -n (prompt_hostname)
-    set_color normal
-    printf ' in '
+  set_color magenta
+  echo -n (prompt_hostname)
+  set_color normal
+  printf '  '
 
-    set_color $fish_color_cwd
-    printf '%s' (prompt_pwd)
-    set_color normal
+  set_color $fish_color_cwd
+  printf '%s' (prompt_pwd)
+  set_color normal
 
-    # Line 2
-    echo
-    if test $VIRTUAL_ENV
-        printf "(%s) " (set_color blue)(basename $VIRTUAL_ENV)(set_color normal)
-    end
-    printf '↪ '
-    set_color normal
+  # Show GIT BRANCH
+  set_color brblack
+  if not test -z (fish_git_prompt)
+    printf '  [  %s ]' (string sub -s 3 -e -1 (fish_git_prompt))
+  end
+  set_color normal
+
+  # Line 2
+  echo
+  if test $VIRTUAL_ENV
+    printf "(%s) " (set_color blue)(basename $VIRTUAL_ENV)(set_color normal)
+  end
+  printf '↪ '
+  set_color normal
 end
