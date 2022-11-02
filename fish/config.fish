10eb3414d6b4f2bd723e95139dd13705367e7731
Paul Kim <44695374+thekpaul@users.noreply.github.com>
2022-11-02 17:40:00 +0900

    Use Miniconda at /home/thekpaul instead of pyenv

--- fish/config.fish
+++ fish/config.fish
@@ -3,7 +3,13 @@ cd
 rvm default
 set -gx PATH /usr/local/texlive/2022/bin/x86_64-linux \
              /usr/local/texlive/2021/bin/x86_64-linux $PATH
-status is-interactive; and pyenv init --path | source
-pyenv init - | source
+# status is-interactive; and pyenv init --path | source
+# pyenv init - | source
 set -gx LC_CTYPE en_US.UTF-8
 set -gx LC_ALL en_US.UTF-8
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+eval /home/thekpaul/miniconda3/bin/conda "shell.fish" "hook" $argv | source
+# <<< conda initialize <<<
+
