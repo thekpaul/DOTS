bbde1d314196afde8f37e79c0aa129e0ad8c3c21
Paul Kim <44695374+thekpaul@users.noreply.github.com>
2023-01-07 23:31:31 +0900

    Refactor Tabspace

--- fish/conf.d/functions.fish
+++ fish/conf.d/functions.fish
@@ -1,19 +1,19 @@
 # FUNCTIONS.fish
 
 function mkcd
-    mkdir $argv
-    cd $argv
+  mkdir $argv
+  cd $argv
 end
 
 function lsa
-    ls -AH --group-directories-first $argv
+  ls -AH --group-directories-first $argv
 end
 
 function lscd
-    cd $argv
-    lsa
+  cd $argv
+  lsa
 end
 
 function sumatra
-    SumatraPDF.exe $argv
+  SumatraPDF.exe $argv
 end
