d3f398d52c23cecd51aa7a20142268b32c8d7885
Paul Kim <44695374+thekpaul@users.noreply.github.com>
2023-04-25 16:25:03 +0900

    Add Automatic Installer Steps for TeXLive, Conda and Fish
    Fish installation step also installs OMF and RVM, specifically tailored
    for use in a Fish shell instance.
    
    TeXLive and Conda (Miniconda) receive respective installer scripts from
    their own servers, which may cause problems. If detecting an erroneous
    task termination, this script does not attempt an installation and
    `echo`es an error message.

--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
 #! /usr/bin/env bash
 
 # 1. Install Necessary Packages, Add PPAs and *Clone This Repo!*
-
 ## Adding PPAs
 sudo apt-add-repository https://packages.microsoft.com/ubuntu/22.04/prod # Microsoft WSL PPA for Ubuntu 22.04
 sudo apt-add-repository ppa:git-core/ppa                                 # Git Core PPA
@@ -18,7 +17,7 @@ sudo apt-get install -y \
   dosfstools kmod iverilog gtkwave zip inkscape openssl cowsay ninja-build
 
 ## Install Neovim 0.8.3 via `.deb` Directly from Repo on Github
-wget "https://github.com/neovim/neovim/releases/download/v0.8.3/nvim-linux64.deb"
+wget https://github.com/neovim/neovim/releases/download/v0.8.3/nvim-linux64.deb
 sudo apt install -y ./nvim-linux64.deb
 
 # 2. Setup Dotfiles and Symlink to `XDG_CONFIG_HOME` (~/.config/)
@@ -30,14 +29,51 @@ gh auth login # Github CLI Login -> Interactive
 
 ## Clone Repo and Make Symlinks
 gh repo clone thekpaul/DOTS ~/Projects/DOTS -- --origin=github # Clone Repo
-ln -sf $HOME/Projects/DOTS/git/config .gitconfig
+ln -sf $HOME/Projects/DOTS/git/config .gitconfig # Git Global Configs go $HOME
 ln -sf $HOME/Projects/DOTS/nvim ~/.config/nvim
 ln -sf $HOME/Projects/DOTS/tmux ~/.config/tmux
 ln -sf $HOME/Projects/DOTS/fish ~/.config/fish
 
-curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
+curl -fLo ~/.config/nvim/autoload --create-dirs \
   https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim # Add Plugin Manager for NVim
 gh repo clone tmux-plugins/tpm ~/.config/tmux/plugins/tpm             # Add Plugin Manager for TMUX
 
 # 3. Setup Fish as Default Shell and add Toolchains
 sudo usermod -s /usr/bin/fish $USER
+
+## OMF
+curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
+
+## RVM Setup
+gpg --keyserver keyserver.ubuntu.com \
+  --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 \
+              7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+curl -sSL https://get.rvm.io | bash -s stable --ruby
+if [ $? -eq 0 ]; then
+  rvm install ruby
+else
+  echo "Error in Installing RVM!"
+fi
+
+# 4. Setup (Latest -> 2023) TeXLive
+wget http://mirror.ctan.org/systems/texlive/tlnet/install-tl-unx.tar.gz
+
+## Only Execute Installation IF Able to Download Installer Tarball
+if [ $? -eq 0 ]; then
+  cd ~ && tar -xzf install-tl-unx.tar.gz
+  cd install-tl-* # Move to Latest Unzip
+  perl install-tl --profile="~/Projects/DOTS/tex/texlive.profile" # Auto Install with Profile
+  rm -rf install-tl-* # Clean Installer and Tarball afterwards
+else
+  echo "Error in Installing TeXLive!"
+fi
+
+# Setup Python in Conda with Miniconda and Add Minimal Packages
+wget -O Miniconda.sh \
+  https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+bash Miniconda.sh
+if [ $? -eq 0 ]; then
+  conda init fish # Initialise in Fish shell
+else
+  echo "Error in Installing Miniconda!"
+fi
