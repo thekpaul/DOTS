f00a4be3e442e550933516e07c7839ed0dacce2c
Paul Kim <44695374+thekpaul@users.noreply.github.com>
2023-07-16 22:02:21 +0900

    Add Filetype Plugin for Git Commits
    Git commit messages are best formatted in the following two rules:
    - Use a 50-character one-liner "Title", used for shortlogs, and
    - Use 72-character lines for body comments (if necessary)
    
    These rules are to be used with hardcoded line breaks during the
    commit process, and helps readability on the terminal considering
    80-character term widths or the `gitk` tool.
    
    This commit sets colored columns in Git commits only, with a 50-char
    colored column for the shortlog (which may be unnecessary as Neovim
    supports a colored one-liner scheme natively) and a 72-char colored
    column for the body.
    
    Note that this commit hosts an extensively and unnecessarily long
    comment - this is used to test if the new filetype plugin script is
    working as expected (which it is!)

---
+++ after/ftplugin/gitcommit.lua
@@ -0,0 +1,7 @@
+--[=[
+	INIT.lua: Initialising Configurations for NeoVim in Lua.
+	Ported and extended from original configurations in legacy vimscript.
+]=]
+
+-- Coloring columns
+vim.opt.colorcolumn = "50,72"
