496ef571b737a9b3f17615fe25ca5690c5fafbdb
Paul Kim <44695374+thekpaul@users.noreply.github.com>
2023-07-17 18:15:12 +0900

    Refactor and Relocate Plugin-Specific Settings
    For now, I will use Vimscript commands alongside Lua commands during
    the refactoring process. The goal is to refactor all Vimscript commands
    to Lua commands.

---
+++ nvim/lua/plugins/ultisnips.lua
@@ -0,0 +1,11 @@
+--[=[
+	PLUGINS_ULTISNIPS.lua:
+	Configurations for the `SirVer/ultisnips` Plugin.
+    Ported and extended from original configurations in legacy vimscript.
+]=]
+
+vim.g.UltiSnipsExpandTrigger = "<tab>"
+vim.g.UltiSnipsJumpForwardTrigger = "<tab>"
+vim.g.UltiSnipsJumpBackwardTrigger = "<s-tab>"
+vim.g.UltiSnipsSnippetDirectories = { vim.fn.stdpath('config') .. "/UltiSnips" }
+vim.g.UltiSnipsEditSplit = "vertical"
---
+++ nvim/lua/plugins/vim-indent-guides.lua
@@ -0,0 +1,18 @@
+--[=[
+	PLUGINS_VIM-INDENT-GUIDES.lua:
+	Configurations for the `preservim/vim-indent-guides` Plugin.
+    Ported and extended from original configurations in legacy vimscript.
+]=]
+
+vim.g.indent_guides_enable_on_vim_startup = true
+vim.g.indent_guides_guide_size = 1
+vim.g.indent_guides_exclude_filetypes = { 'help', 'startify' }
+
+-- Manually Set Foreground Colors matching `lcs` Tabs in Indent Guides
+vim.g.indent_guides_auto_colors = false
+vim.cmd [[
+	autocmd VimEnter,Colorscheme *
+		\ :hi IndentGuidesOdd  ctermfg=59 guifg=#727272
+	autocmd VimEnter,Colorscheme *
+		\ :hi IndentGuidesEven ctermfg=59 guifg=#727272
+]]
