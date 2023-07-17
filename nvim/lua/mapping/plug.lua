523a8c6d7f91576e36cff860d28dc7aa713fa675
Paul Kim <44695374+thekpaul@users.noreply.github.com>
2023-07-17 19:41:48 +0900

    Retab Lua Files to Hard Tabs
    Experimental: Let's try hard-tabbing Lua files!

--- nvim/lua/mapping/plug.lua
+++ nvim/lua/mapping/plug.lua
@@ -1,11 +1,11 @@
 --[=[
 	MAPPING_PLUG.lua:
 	Mappings for the `junegunn/vim-plug` Plugin.
-    Ported and extended from original configurations in legacy vimscript.
+	Ported and extended from original configurations in legacy vimscript.
 ]=]
 
 vim.cmd [[
-    " nnoremap <F12> :PlugInstall<CR>  | " Press F12 to Install new Plugins
-    " nnoremap <C-F12> :PlugClean<CR>  | " Press Ctrl+F12 to Delete Plugins
-    " nnoremap <S-F12> :PlugUpdate<CR> | " Press Shift+F12 to Update new Plugins
+	" nnoremap <F12> :PlugInstall<CR>  | " Press F12 to Install new Plugins
+	" nnoremap <C-F12> :PlugClean<CR>  | " Press Ctrl+F12 to Delete Plugins
+	" nnoremap <S-F12> :PlugUpdate<CR> | " Press Shift+F12 to Update new Plugins
 ]]
