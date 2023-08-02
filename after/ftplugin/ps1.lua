02c5754e3454610715ce2c0c0a9bf53c3b9efd74
Paul Kim <44695374+thekpaul@users.noreply.github.com>
2023-08-02 09:13:14 +0900

    FEAT: Add Filetype Plugin for PowerShell Scripts
    The `.ps1` filetype is used for PowerShell scripts, notably the
    startup profile scripts.
    This commit adds a simple configurations file for the `.ps1` filetype
    that sets tab width as 4 spaces.
    
    Signed-off-by: Paul Kim <44695374+thekpaul@users.noreply.github.com>

---
+++ after/ftplugin/ps1.lua
@@ -0,0 +1,10 @@
+--[=[
+	FTPLUGIN_PS1.lua:
+	Configurations for the `ps1` (PowerShell) filetype.
+]=]
+
+-- Local Tab/Space Settings
+vim.opt_local.shiftwidth = 4
+vim.opt_local.tabstop = 4
+vim.opt_local.softtabstop = 4
+vim.opt_local.smarttab = true
