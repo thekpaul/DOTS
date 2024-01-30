f6225ac964c274cad5f3cffdb99cbe6d39970812
Paul Kim <44695374+thekpaul@users.noreply.github.com>
2024-01-30 20:11:29 +0900

    FEAT: Cross-platform Miniconda Script and `lsa`
    Note that we are only considering user-local Miniconda3 installations
    to either macOS (Darwin) or Linux at this time.
    Support for other operating systems or installation directories will be
    added in the future.
    
    Also note that the `lsa` command for macOS systems require the
    `coreutils` homebrew formula, which provides the GNU `ls` executable.
    
    Signed-off-by: Paul Kim <44695374+thekpaul@users.noreply.github.com>

--- config.fish
+++ config.fish
@@ -6,11 +6,26 @@ if not set -q VIRTUAL_ENV_DISABLE_PROMPT
   set -g VIRTUAL_ENV_DISABLE_PROMPT true
 end
 
+if [ (uname) = "Darwin" ]
+  if test -f /Users/thekpaul/miniconda3/bin/conda
+    eval /Users/thekpaul/miniconda3/bin/conda "shell.fish" "hook" $argv | source
+  else
+    if test -f "/Users/thekpaul/miniconda3/etc/fish/conf.d/conda.fish"
+      . "/Users/thekpaul/miniconda3/etc/fish/conf.d/conda.fish"
+    else
+      set -x PATH "/Users/thekpaul/miniconda3/bin" $PATH
+    end
+  end
+else if [ (uname) = "Linux" ]
+  if test -f /home/thekpaul/miniconda3/bin/conda
+    eval /home/thekpaul/miniconda3/bin/conda "shell.fish" "hook" $argv | source
+  end
+else
+  echo "Unsupported Operating System: Please report to upstream!"
+end
+
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
-if test -f /home/thekpaul/miniconda3/bin/conda
-    eval /home/thekpaul/miniconda3/bin/conda "shell.fish" "hook" $argv | source
-end
 # <<< conda initialize <<<
 
 # kill the right prompt __conda_add_prompt 😠
