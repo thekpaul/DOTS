486f80b07f54a1c44e87d050cfadd72bd41f3ce8
Paul Kim <44695374+thekpaul@users.noreply.github.com>
2024-02-11 23:19:21 +0900

    FEAT: Add Condition for VS Code's Neovim Extension
    The Neovim extension for VS Code sources the same Neovim configurations
    from this repository, which may cause undesireable side effects in the
    VS Code environment due to compatibility issues.
    As such, most configurations are now wrapped in a conditional statement
    to check for the VS Code environment by default.
    Note that while most configurations are currently unused in VS Code,
    cursor selection options and virtual edit configurations are retained.
    
    This commit is not yet tested on Linux or Mac environments.
    However, as both Neovim and its embedded Lua support are both
    platform-independent, stable results are expected on both environments
    as well.
    
    Signed-off-by: Paul Kim <44695374+thekpaul@users.noreply.github.com>

--- init.lua
+++ init.lua
@@ -7,6 +7,14 @@
 vim.opt.exrc = true
 vim.opt.secure = true
 
+--[=[
+	NOTE: This configuration is sourced in both vanilla NeoVim and VS Code,
+	via the "VSCode Neovim" extension. As such, all prior settings are now
+	wrapped in a conditional statement to check for VS Code usage by default.
+	This will be sorted in future commits to replicate the current Neovim
+	setup in VS Code as well.
+]=]
+
 -- Behaviour changes to `mswin`
 vim.opt.selection  = 'exclusive'
 vim.opt.selectmode = { 'mouse', 'key' }
@@ -16,126 +24,142 @@ vim.opt.keymodel   = { 'startsel', 'stopsel' }
 -- Virtual Edits
 vim.opt.virtualedit = 'onemore' -- For Consistency in Selective AutoClosing
 
--- Mouse usage
-vim.o.mouse = 'a'
+if not vim.g.vscode then
+	-- Mouse usage
+	vim.o.mouse = 'a'
 
--- Select-mode Windows-like clipboard
-vim.cmd [[
-	vnoremap <C-x> "+x
-	vnoremap <C-c> "+y
-	cnoremap <C-v> <C-r>+
-	exe 'ino <script> <C-V>' paste#paste_cmd['i']
-]]
+	-- Select-mode Windows-like clipboard
+	vim.cmd [[
+		vnoremap <C-x> "+x
+		vnoremap <C-c> "+y
+		cnoremap <C-v> <C-r>+
+		exe 'ino <script> <C-V>' paste#paste_cmd['i']
+	]]
 
--- Windows-like un/redo
-vim.cmd [[
-	nnoremap <C-z> u
-	nnoremap <C-y> <C-r>
-	inoremap <C-z> <Esc>ui
-	inoremap <C-y> <Esc><C-r>i
-]]
+	-- Windows-like un/redo
+	vim.cmd [[
+		nnoremap <C-z> u
+		nnoremap <C-y> <C-r>
+		inoremap <C-z> <Esc>ui
+		inoremap <C-y> <Esc><C-r>i
+	]]
 
--- Saving without whitespaces | TODO: Fix reset of `/` register
-vim.cmd [[
-	inoremap <C-s> <Esc>:%s/\s\+$//e<CR>:let @/=""<CR>:update<CR>a
-	nnoremap <C-s> :%s/\s\+$//e<CR>:let @/=""<CR>:update<CR>
-]]
+	-- Saving without whitespaces | TODO: Fix reset of `/` register
+	vim.cmd [[
+		inoremap <C-s> <Esc>:%s/\s\+$//e<CR>:let @/=""<CR>:update<CR>a
+		nnoremap <C-s> :%s/\s\+$//e<CR>:let @/=""<CR>:update<CR>
+	]]
 
--- Delete, yank, select a document
-vim.cmd [[
-	nnoremap dad ggVGd
-	nnoremap yad %y
-	nnoremap vad ggVG
-]]
+	-- Delete, yank, select a document
+	vim.cmd [[
+		nnoremap dad ggVGd
+		nnoremap yad %y
+		nnoremap vad ggVG
+	]]
 
--- Undo files
-vim.opt.undofile = false
+	-- Undo files
+	vim.opt.undofile = false
 
--- Set plugin loading according to filetype
-vim.cmd [[
-	filetype off
-	filetype plugin indent on
-]]
+	-- Set plugin loading according to filetype
+	vim.cmd [[
+		filetype off
+		filetype plugin indent on
+	]]
 
--- Line numbers
-vim.opt.number = true
+	-- Line numbers
+	vim.opt.number = true
 
--- Default tab behaviour
-vim.opt.tabstop     = 2
-vim.opt.softtabstop = 2
-vim.opt.shiftwidth  = 2
-vim.opt.expandtab   = true
-vim.opt.smarttab    = true
-vim.opt.autoindent  = true
+	-- Default tab behaviour
+	vim.opt.tabstop     = 2
+	vim.opt.softtabstop = 2
+	vim.opt.shiftwidth  = 2
+	vim.opt.expandtab   = true
+	vim.opt.smarttab    = true
+	vim.opt.autoindent  = true
 
--- Line wrapping
-vim.opt.wrap = false
-vim.opt.sidescroll = 8
+	-- Line wrapping
+	vim.opt.wrap = false
+	vim.opt.sidescroll = 8
 
--- Search case-sensitivity
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
+	-- Search case-sensitivity
+	vim.opt.ignorecase = true
+	vim.opt.smartcase = true
 
--- Current cursor line/column highlighting
-vim.opt.cursorline   = true
-vim.opt.cursorcolumn = true
+	-- Current cursor line/column highlighting
+	vim.opt.cursorline   = true
+	vim.opt.cursorcolumn = true
 
--- Respect 24-bit RGB colors in TUI (favours `gui` values over `cterm`)
-vim.opt.termguicolors = true
+	-- Respect 24-bit RGB colors in TUI (favours `gui` values over `cterm`)
+	vim.opt.termguicolors = true
 
--- Show sign column so that text doesn't shift
--- vim.opt.signcolumn = "yes"
+	-- Show sign column so that text doesn't shift
+	-- vim.opt.signcolumn = "yes"
 
--- Coloring columns
-vim.opt.colorcolumn = { "80", "120" }
+	-- Coloring columns
+	vim.opt.colorcolumn = { "80", "120" }
 
--- Encoding and Language Settings
-vim.opt.encoding = "utf8"
-vim.opt.fileencodings = "utf8"
+	-- Encoding and Language Settings
+	vim.opt.encoding = "utf8"
+	vim.opt.fileencodings = "utf8"
 
--- Indentation and Line Breaks
-vim.opt.breakindent = true
-vim.opt.linebreak = true
+	vim.api.nvim_create_autocmd( "UIEnter",
+		{
+			once = true,
+			callback = function()
+				vim.cmd [[
+					runtime delmenu.vim
+					set langmenu=en_US.utf8
+					let $LANG = 'en_US'
+					runtime menu.vim
+				]]
+			 -- require "ginit" -- Load GUI settings from separate file
+			end
+		}
+	)
 
--- Add Backslash to Keyword List
-vim.opt.iskeyword:append { '\\' }
+	-- Indentation and Line Breaks
+	vim.opt.breakindent = true
+	vim.opt.linebreak = true
 
--- Split new panes to right & below
-vim.opt.splitright = true
-vim.opt.splitbelow = true
+	-- Add Backslash to Keyword List
+	vim.opt.iskeyword:append { '\\' }
 
--- Fold Method
-vim.opt.foldmethod = "manual"
+	-- Split new panes to right & below
+	vim.opt.splitright = true
+	vim.opt.splitbelow = true
 
--- List and List Characters
-vim.opt.list = true
-vim.opt.listchars = {
-	tab   = "I->",
-	trail = "X",
- --	nbsp  = "␣",
- --	eol   = "↲",
-}
+	-- Fold Method
+	vim.opt.foldmethod = "manual"
 
--- Scroll Off
-vim.opt.scrolloff = 10
+	-- List and List Characters
+	vim.opt.list = true
+	vim.opt.listchars = {
+		tab   = "I->",
+		trail = "X",
+	 --	nbsp  = "␣",
+	 --	eol   = "↲",
+	}
 
--- Providers
-vim.g.loaded_perl_provider = 0
+	-- Scroll Off
+	vim.opt.scrolloff = 10
 
--- Default LaTeX Flavor
-vim.g.tex_flavor = "latex"
+	-- Providers
+	vim.g.loaded_perl_provider = 0
 
--- Legacy Vimscript Configurations from `init.vim`
+	-- Default LaTeX Flavor
+	vim.g.tex_flavor = "latex"
 
-vim.cmd [[
-autocmd VimEnter,Colorscheme * :hi Comment gui=italic
-au! BufRead,BufNewFile *.h setfiletype c
+	-- Legacy Vimscript Configurations from `init.vim`
 
-" Jump Mechanism
-nnoremap <silent> <C-j> /<++><CR>:let @/ = ""<CR>4"_xi
-inoremap <silent> <C-j> <Esc>/<++><CR>:let @/ = ""<CR>4"_xi
+	vim.cmd [[
+	autocmd VimEnter,Colorscheme * :hi Comment gui=italic
+	au! BufRead,BufNewFile *.h setfiletype c
 
-" AutoClose {
+	" Jump Mechanism
+	nnoremap <silent> <C-j> /<++><CR>:let @/ = ""<CR>4"_xi
+	inoremap <silent> <C-j> <Esc>/<++><CR>:let @/ = ""<CR>4"_xi
+
+	" AutoClose {
 
 	inoremap ( ()<lt>++><Esc>5ha
 	inoremap [ []<lt>++><Esc>5ha
@@ -163,9 +187,9 @@ inoremap <silent> <C-j> <Esc>/<++><CR>:let @/ = ""<CR>4"_xi
 		vmap " di"<Esc>p
 	endif
 
-" }
+	" }
 
-" PLUGINS with Vim-Plug as Plugin Manager {
+	" PLUGINS with Vim-Plug as Plugin Manager {
 
 	call plug#begin(stdpath('config') . '/plugged')
 		Plug 'mhinz/vim-startify', { 'on':  'Startify' }
@@ -187,37 +211,39 @@ inoremap <silent> <C-j> <Esc>/<++><CR>:let @/ = ""<CR>4"_xi
 		endif
 	call plug#end()
 
-" }
+	" }
 
-]]
+	]]
 
--- Vim-Plug Custom Mappings
-require('mapping.plug')
+	-- Vim-Plug Custom Mappings
+	require('mapping.plug')
 
--- BarBar: Buffer/Tabline
-require('plugins.barbar')
-require('mapping.barbar')
+	-- BarBar: Buffer/Tabline
+	require('plugins.barbar')
+	require('mapping.barbar')
 
--- Vim-Airline: Lightweight Statusline
-require('plugins.vim-airline')
+	-- Vim-Airline: Lightweight Statusline
+	require('plugins.vim-airline')
 
--- Seoul256: Colorscheme
-require('plugins.seoul256')
+	-- Seoul256: Colorscheme
+	require('plugins.seoul256')
 
--- VimTeX: LaTeX with (Neo)Vim
-require('plugins.vimtex')
+	-- VimTeX: LaTeX with (Neo)Vim
+	require('plugins.vimtex')
 
--- Startify: Startup Screen
-require('plugins.startify')
+	-- Startify: Startup Screen
+	require('plugins.startify')
 
--- Vim-Indent-Guides: Vertical Indent Guide ColorColumns
-require('plugins.vim-indent-guides')
+	-- Vim-Indent-Guides: Vertical Indent Guide ColorColumns
+	require('plugins.vim-indent-guides')
 
--- UltiSnips: Snippets Engine
-require('plugins.ultisnips')
+	-- UltiSnips: Snippets Engine
+	require('plugins.ultisnips')
 
--- NVim-Treesitter: Parsing Library
-require('plugins.treesitter')
+	-- NVim-Treesitter: Parsing Library
+	require('plugins.treesitter')
 
--- Signify: VCS Indicators in (N)Vim Line-number Sidebar
-require('plugins.signify')
+	-- Signify: VCS Indicators in (N)Vim Line-number Sidebar
+	require('plugins.signify')
+
+end
