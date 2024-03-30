f720f495c5729ed300e5fac3d55f6892d2a07fc9
Paul Kim <44695374+thekpaul@users.noreply.github.com>
2024-03-31 01:13:59 +0900

    REFACTOR: Substitute Imported Plugin Module
    Signed-off-by: Paul Kim <44695374+thekpaul@users.noreply.github.com>

---
+++ nvim/lua/plugins/indent-blankline.lua
@@ -0,0 +1,10 @@
+--[=[
+	INDENT-BLANKLIKE.lua:
+	Configurations for the `lukas-reineke/indent-blankline.nvim` Plugin.
+]=]
+
+return {
+	"lukas-reineke/indent-blankline.nvim",
+	main = "ibl",
+	opts = {}
+}
