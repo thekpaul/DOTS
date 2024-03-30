e7c6b0c9d7e7fbee0347b6f67c44558320c172a2
Paul Kim <44695374+thekpaul@users.noreply.github.com>
2024-03-31 01:13:59 +0900

    REFACTOR: Clean NeoVim Mappings into Plugins Dir
    The `vim-plug` mappings are unused, so its file was removed completely.
    The `barbar` mappings are appended to its plugin configurations file.
    
    Signed-off-by: Paul Kim <44695374+thekpaul@users.noreply.github.com>

--- nvim/lua/plugins/barbar.lua
+++ nvim/lua/plugins/barbar.lua
@@ -142,3 +142,46 @@ vim.cmd [[ " Temporary custom highlight groups
 	autocmd VimEnter,Colorscheme * :hi BufferTabpages    guibg=#565656 guifg=#ffffff
 	autocmd VimEnter,Colorscheme * :hi BufferTabpagesSep guibg=#565656 guifg=#ffffff
 ]]
+
+-- Mappings for the `romgrk/barbar.nvim` Plugin.
+vim.cmd [[
+	" Move to previous/next
+	nnoremap <silent> ,, :BufferPrevious<CR>
+	nnoremap <silent> ,. :BufferNext<CR>
+
+	" Re-order to previous/next
+	nnoremap <silent> ,< :BufferMovePrevious<CR>
+	nnoremap <silent> ,> :BufferMoveNext<CR>
+
+	" Goto buffer in position...
+	nnoremap <silent> ,1 :BufferGoto 1<CR>
+	nnoremap <silent> ,2 :BufferGoto 2<CR>
+	nnoremap <silent> ,3 :BufferGoto 3<CR>
+	nnoremap <silent> ,4 :BufferGoto 4<CR>
+	nnoremap <silent> ,5 :BufferGoto 5<CR>
+	nnoremap <silent> ,6 :BufferGoto 6<CR>
+	nnoremap <silent> ,7 :BufferGoto 7<CR>
+	nnoremap <silent> ,8 :BufferGoto 8<CR>
+	nnoremap <silent> ,9 :BufferLast<CR>
+
+	" Pin/unpin buffer
+	nnoremap <silent> ,p :BufferPin<CR>
+
+	" Close buffer
+	nnoremap <silent> ,c :BufferClose<CR>
+
+	" Wipeout buffer :BufferWipeout<CR>
+	" Magic buffer-picking mode :BufferPick<CR>
+	" Close commands
+	" - :BufferCloseAllButCurrent<CR>
+	" - :BufferCloseAllButPinned<CR>
+	" - :BufferCloseAllButCurrentOrPinned<CR>
+	" - :BufferCloseBuffersLeft<CR>
+	" - :BufferCloseBuffersRight<CR>
+
+	" Sort automatically by...
+	nnoremap <silent> ,bb :BufferOrderByBufferNumber<CR>
+	nnoremap <silent> ,bd :BufferOrderByDirectory<CR>
+	nnoremap <silent> ,bl :BufferOrderByLanguage<CR>
+	nnoremap <silent> ,bw :BufferOrderByWindowNumber<CR>
+]]
