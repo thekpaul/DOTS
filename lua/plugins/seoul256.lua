e7e9216ec28f57c5731c547af61daa5f6688d9d1
Paul Kim <44695374+thekpaul@users.noreply.github.com>
2024-04-05 03:39:18 +0900

    FEAT: Lua API to Set `EndOfBuffer` Highlight Group
    The `nvim_get_hl` API is used to retrieve the baseline `Normal`
    highlight group.
    This is then used to create a table for a new highlight group,
    `HideTilde`, which inherits the background color from `Normal`
    and sets the foreground color to the background color.
    The `HideTilde` highlight group is then set as the `EndOfBuffer`
    highlight group, which is used to hide the buffer end tildes.
    
    Unfortunately, the `EndOfBuffer` highlight group is currently
    entirely overwritten with hard-coded values retrieved from the
    `Normal` highlight group on startup.
    This may cause the `EndOfBuffer` highlight group to be incorrect
    if the `Normal` highlight group is modified (i.e. by a colorscheme)
    after the `EndOfBuffer` highlight group is set.
    
    Also changes the `init` key to `config` to suppress unexpected
    behaviours that may occur when the plugin is loaded.
    The `config` key is used to avoid calling a possible `setup()` function
    embedded in the plugin.
    As this is a Vimscript plugin, there is no `setup()` function to call,
    but it is good practice to use the `config` key to avoid unexpected
    `setup()` function calls.
    
    Signed-off-by: Paul Kim <44695374+thekpaul@users.noreply.github.com>

--- lua/plugins/seoul256.lua
+++ lua/plugins/seoul256.lua
@@ -8,12 +8,18 @@ return {
 	'junegunn/seoul256.vim',
 	lazy = false,
 	priority = 1000,
-	init = function()
+	config = function()
 		vim.g.seoul256_background = 234
 		vim.cmd.colorscheme 'seoul256'
-		vim.cmd [[
-			highlight EndOfBuffer guifg=bg   | " Hide Buffer End Tildes for GUI
-			highlight EndOfBuffer ctermfg=bg | " Hide Buffer End Tildes for Terminal
-		]]
+
+		-- Hide buffer end tildes
+		-- Source: https://www.reddit.com/r/neovim/comments/yexeil/comment/iu0lbgt/?snippet=1_48_242
+		local DefNormal = vim.api.nvim_get_hl(0, { name = "Normal" }) -- `Normal` highlight group
+		local HideTilde = vim.tbl_extend('force', -- Overwrite from right-most table
+			{}, -- Baseline empty table to fill necessary key-value pairs
+			DefNormal, -- Get all pairs from `Normal` (keep backgrounds)
+			{ fg = DefNormal.bg, ctermfg = DefNormal.ctermbg } -- Overwrite foregrounds
+		)
+		vim.api.nvim_set_hl(0, "EndOfBuffer", HideTilde)
 	end
 }
