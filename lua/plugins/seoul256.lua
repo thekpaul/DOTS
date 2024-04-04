c7fd6c1539bbf68d99062833cfeddd27e4dddee3
Paul Kim <44695374+thekpaul@users.noreply.github.com>
2024-04-05 03:45:10 +0900

    FEAT: Use `fillchars` to Unset EOB Character
    The end-of-buffer characters are set by the `fillchars.eob` option,
    which defaults to the "tilde" (`~`) character.
    This is unset in the top-level user configuration file `init.lua`,
    which removes the need for highlight group modifications.
    
    Signed-off-by: Paul Kim <44695374+thekpaul@users.noreply.github.com>

--- lua/plugins/seoul256.lua
+++ lua/plugins/seoul256.lua
@@ -11,13 +11,5 @@ return {
 	config = function()
 		vim.g.seoul256_background = 234
 		vim.cmd.colorscheme 'seoul256'
-
-		-- Hide buffer end tildes
-		local DefNormal = vim.api.nvim_get_hl(0, { name = "Normal" })
-		vim.api.nvim_set_hl(0, "EndOfBuffer", {
-			-- NOTE: Only values from STARTUP are applied!
-			fg      = DefNormal.bg,     -- Hide in GUI
-			ctermfg = DefNormal.ctermbg -- Hide in TUI
-		})
 	end
 }
