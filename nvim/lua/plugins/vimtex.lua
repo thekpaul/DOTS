f542f0a18a881219709dbe7fa588a600467b842a
Paul Kim <44695374+thekpaul@users.noreply.github.com>
2024-04-05 03:45:10 +0900

    FEAT: Modify Configurations for VimTeX Plugin
    The filetype-based lazy loading is removed, as it may interfere with
    global functions such as `:VimtexInverseSearch`.
    
    The `config` block has been renamed to the `init` block, which is
    recommended by the VimTeX plugin maintainer(s).
    
    While not used, the `vim.g.vimtex_view_general_viewer` option has been
    enabled and changed to `wslview` to better suite possible use cases in
    WSL environments.
    Future commits will elaborate on this option to support multiple
    operating systems and typesetting environments.
    
    Lastly, the `vim.g.vimtex_imaps_enabled` option has been disabled,
    as the default settings interfered with custom maps in insert mode.
    While commented-out currently, the `vim.g.vimtex_imaps_leader` option
    will be changed to the `@` key to complement the current custom maps.
    
    Signed-off-by: Paul Kim <44695374+thekpaul@users.noreply.github.com>

--- nvim/lua/plugins/vimtex.lua
+++ nvim/lua/plugins/vimtex.lua
@@ -6,12 +6,14 @@
 
 return {
 	'lervag/vimtex',
-	ft = "tex",
-	config = function()
+ --	ft = "tex", -- Load on startup for global functions!
+	init = function()
 		vim.g.vimtex_indent_enabled = false
-	 --	vim.g.vimtex_view_general_viewer = '.local/bin/sumatrapdf.sh'
+		vim.g.vimtex_view_general_viewer = 'wslview' -- TODO: Change for other OSes!
 	 --	vim.g.vimtex_view_general_options = '-reuse-instance -forward-search @tex @line @pdf'
 	 --	vim.g.vimtex_compiler_method = 'generic'
 	 --	vim.g.vimtex_compiler_generic = { 'command' : 'make' }
+		vim.g.vimtex_imaps_enabled = false
+	 --	vim.g.vimtex_imaps_leader = '@' -- TODO: Check how `imaps` work when enabled!
 	end
 }
