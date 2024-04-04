2f4c8dca60d7d7a910e471527b7026c17793ca21
Paul Kim <44695374+thekpaul@users.noreply.github.com>
2024-04-05 03:45:10 +0900

    REFACTOR: Use Self-hosting Source for `tpope/**`
    Applies to `vim-fugitive` (renamed to `fugitive`) and `vim-surround`
    (renamed to `surround`).
    
    Signed-off-by: Paul Kim <44695374+thekpaul@users.noreply.github.com>

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -3,8 +3,8 @@ return {
 		'nvim-tree/nvim-web-devicons',
 		lazy = true -- API plugin!
 	},
-	'tpope/vim-fugitive',
-	'tpope/vim-surround',
-	'wakatime/vim-wakatime',
+	{ url = 'https://tpope.io/vim/fugitive.git'},
+	{ url = 'https://tpope.io/vim/surround.git'},
 	{ url = 'https://tpope.io/vim/abolish.git'},
+	'wakatime/vim-wakatime',
 }
