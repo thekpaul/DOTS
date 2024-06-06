db0443f8a8801a14425284eabebd9f10f890fcb0
Paul Kim <44695374+thekpaul@users.noreply.github.com>
2024-06-06 19:45:27 +0900

    REFACTOR: Italicise Comments in Colorscheme Setup
    Not related to the colorscheme plugin used here (`seoul256`), but still
    useful to place with the colorscheme config function for sourced-time
    clarity.
    
    Signed-off-by: Paul Kim <44695374+thekpaul@users.noreply.github.com>

--- nvim/lua/plugins/seoul256.lua
+++ nvim/lua/plugins/seoul256.lua
@@ -11,5 +11,16 @@ return {
 	config = function()
 		vim.g.seoul256_background = 234
 		vim.cmd.colorscheme 'seoul256'
+
+		-- Show comments as ITALIC (works well with cursive fonts)
+		--   -> Apply AFTER Colorscheme!
+		local oldComment = vim.api.nvim_get_hl(0, { name = "Comment" })
+		local newComment = vim.tbl_extend('force',
+			{}, -- Baseline empty table to fill necessary key-value pairs
+			oldComment, -- Get all pairs from original `Comment` group
+			{ italic = true } -- Overwrite as italics
+		)
+
+		vim.api.nvim_set_hl(0, "Comment", newComment)
 	end
 }
