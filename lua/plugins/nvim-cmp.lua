ff464d0586b30c406f5e0cdcdb6df4941de88c76
Paul Kim <44695374+thekpaul@users.noreply.github.com>
2024-06-08 01:09:50 +0900

    FEAT: Initial Configs for Completion (`nvim-cmp`)
    Also uses the `LuaSnip` plugin for extended suggestions from pre-defined
    snippets.
    
    Signed-off-by: Paul Kim <44695374+thekpaul@users.noreply.github.com>

---
+++ lua/plugins/nvim-cmp.lua
@@ -0,0 +1,47 @@
+--[=[
+	PLUGINS_NVIM-CMP.lua:
+	Configurations for the `hrsh7th/nvim-cmp` Plugin.
+]=]
+
+return {
+	"hrsh7th/nvim-cmp",
+	event = "InsertEnter",
+	dependencies = {
+		"hrsh7th/cmp-buffer",          -- source for text in buffer
+		"hrsh7th/cmp-path",            -- source for file system paths
+		"L3MON4D3/LuaSnip",            -- snippet engine using Lua
+		"saadparwaiz1/cmp_luasnip",    -- for autocompletion
+		"rafamadriz/friendly-snippets" -- useful snippets (VS Code format)
+	},
+	config = function()
+		local cmp = require("cmp")
+		local luasnip = require("luasnip")
+
+		-- load VS Code style snippets from installed plugins
+		require("luasnip.loaders.from_vscode").lazy_load()
+
+		cmp.setup({
+			completion = { -- Completion Menu -> `:h completeopt`
+				completeopt = "menu, menuone, preview, noselect"
+			},
+			snippet = { -- Interaction with snippet engine
+				expand = function(args)
+					luasnip.lsp_expand(args.body)
+				end
+			},
+			mapping = cmp.mapping.preset.insert({
+				["<S-Tab>"]   = cmp.mapping.select_prev_item(),
+				["<Tab>"]     = cmp.mapping.select_next_item(),
+				["<C-Space>"] = cmp.mapping.complete(),
+				["<C-c>"]     = cmp.mapping.abort(),
+				["<CR>"]      = cmp.mapping.confirm({ select = false })
+			}),
+			sources = cmp.config.sources({
+				-- Order here is respected
+				{ name = "luasnip" }, -- snippets
+				{ name = "buffer" },  -- current buffer text
+				{ name = "path" }     -- filesystem paths
+			})
+		})
+	end
+}
