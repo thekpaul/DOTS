dafab34ee4ab476e23c600297234a6ff02aba0c2
Paul Kim <44695374+thekpaul@users.noreply.github.com>
2024-06-08 02:34:32 +0900

    FEAT: LSP Configurations with Added Completions
    The `mason` plugin manager specialises in LSP, DAP, linter and
    formatter plugins for Neovim.
    This plugin manager is bootstrapped as a plugin alongside LSP-focused
    plugins such as `mason-lspconfig` and `nvim-lspconfig` to integrate LSP
    functionalities into Neovim runtimes as smooth as possible.
    
    `nvim-telescope` and `nvim-cmp` are integrated with LSP functionalities
    for an even smoother experience - `nvim-telescope` is used to check for
    diagnostics provided by the LSP, while `nvim-cmp` is further extended
    for completions provided by the LSP.
    In particular, the LSP support in `nvim-cmp` is followed up by the usage
    of `cmp-nvim-lsp`, an additional plugin specifically for LSP
    integrations with `nvim-cmp`.
    
    Note that this is an initial configuration; further changes may be
    introduced with the introduction of more LSPs.
    In particular, the `clangd` LSP for C/C++ may require additional support
    for its attach handler.
    
    Signed-off-by: Paul Kim <44695374+thekpaul@users.noreply.github.com>

---
+++ lua/plugins/mason.lua
@@ -0,0 +1,32 @@
+--[=[
+	PLUGINS_MASON.lua:
+	Configurations for the `williamboman/mason.nvim` Plugin, with dependencies.
+]=]
+
+return {
+	{
+		"williamboman/mason.nvim",
+		opts = {
+			ui = {
+				icons = {
+					package_installed   = "✓ ",
+					package_pending     = "➜ ",
+					package_uninstalled = "✗ "
+				}
+			}
+		}
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		dependencies = {
+			"williamboman/mason.nvim" -- Loaded before!
+		},
+		opts = {
+			ensure_installed = {
+				"lua_ls",
+				"clangd"
+			},
+			automatic_installation = true
+		}
+	}
+}
