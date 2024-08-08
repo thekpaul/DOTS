646ceb7ab7cbb840fa5c7df65df45029e8ec3db3
Paul Kim <44695374+thekpaul@users.noreply.github.com>
2024-08-08 21:45:25 +0900

    REFACTOR: Display File Owner in `tree` View
    This is useful on servers where I may copy over files from other users.
    
    Signed-off-by: Paul Kim <44695374+thekpaul@users.noreply.github.com>

--- fish/conf.d/functions.fish
+++ fish/conf.d/functions.fish
@@ -29,8 +29,8 @@ end
 
 if type -q eza
   function tree
-    lsa --tree --level=1 -lo --no-permissions --no-user \
-        --time-style "+%y/%m/%d %H:%M"  $argv
+    lsa --tree --level=1 -lo --no-permissions --time-style "+%y/%m/%d %H:%M" \
+        $argv
   end
 end
 
