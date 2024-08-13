8d38b068925cc303975cecaf33c34c16a63ef8aa
Paul Kim <44695374+thekpaul@users.noreply.github.com>
2024-08-13 23:39:17 +0900

    FEAT: List Available `ic`-scripts by Default
    This requires `ic`-scripts to be provided as executable commands.
    
    Signed-off-by: Paul Kim <44695374+thekpaul@users.noreply.github.com>

--- fish/functions/ic.fish
+++ fish/functions/ic.fish
@@ -1,5 +1,12 @@
 function ic --description 'IC Scripts Wrapper for Fish'
-  if type -q ic$argv
+  if not count $argv > /dev/null
+    for command in (complete -C ic | grep command)
+      string match -r "ic\d+" $command
+    end
+  else if set -q argv[2]
+    echo "Too many arguments: only one is supported at this time."
+    return
+  else if type -q ic$argv
   # echo (which ic$argv)
     exec bash -c "source $(which ic$argv); exec fish"
   else
