31fc42ec9db05d334359d31772acc08605cbb5c2
Paul Kim <44695374+thekpaul@users.noreply.github.com>
2024-09-02 22:36:15 +0900

    FIX: Open New `fish` Shell with Conda Env Intact
    This allows the new fish-shell instance with all `conda`-provided
    programs and environmental sugar intact.
    The Conda environment is deactivated and reactivated post-startup to
    ensure that all programs are refreshed and re-prioritised.
    
    Signed-off-by: Paul Kim <44695374+thekpaul@users.noreply.github.com>

--- conf.d/pdk.fish
+++ conf.d/pdk.fish
@@ -15,13 +15,12 @@ if string match -qr 'pdk*' (prompt_hostname)
       end
     else if set -q argv[2]
       echo "Too many arguments: only one is supported at this time."
-      return
+      return 1
     else if type -q ic$argv
       set FISH_PATH (which fish)
       if type -q conda; and set -q CONDA_DEFAULT_ENV
         set CONDA_ENV $CONDA_DEFAULT_ENV
-        conda deactivate
-        exec bash -c "source $(which ic$argv); exec $FISH_PATH -C 'conda activate $CONDA_ENV'"
+        exec bash -c "source $(which ic$argv); exec $FISH_PATH -C 'conda deactivate; conda activate $CONDA_ENV'"
       else
         exec bash -c "source $(which ic$argv); exec $FISH_PATH"
       end
