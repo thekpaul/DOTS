da0d2103c9ae9baada254fb98cd3fc7e652975a4
Paul Kim <44695374+thekpaul@users.noreply.github.com>
2024-09-04 16:02:37 +0900

    STYLE: Remove Default Level from `tree` Function
    Having a default level removes the possibility of displaying all file
    trees open, i.e. without a level option.
    This commit removes the default level option embedded in the function
    call, which allows for a full tree-view.
    Levels can be applied with the `-L` option.
    
    Signed-off-by: Paul Kim <44695374+thekpaul@users.noreply.github.com>

--- conf.d/functions.fish
+++ conf.d/functions.fish
@@ -29,7 +29,7 @@ end
 
 if type -q eza
   function tree
-    lsa --tree --level=1 -lo --no-permissions --time-style "+%y/%m/%d %H:%M" \
+    lsa --tree -lo --no-permissions --time-style "+%y/%m/%d %H:%M" \
         $argv
   end
 end
