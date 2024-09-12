5f7601f8aff9586ba1154badf08d478e3484ea85
Paul Kim <44695374+thekpaul@users.noreply.github.com>
2024-09-12 13:58:03 +0900

    REFACTOR: Redirect Error Messages to `stderr`
    The function `ic` now prints error messages to `stderr` and returns a
    non-zero exit code for failed function calls.
    
    Also removes a redundant `and`.
    
    Signed-off-by: Paul Kim <44695374+thekpaul@users.noreply.github.com>

--- conf.d/ruby.fish
+++ conf.d/ruby.fish
@@ -1,6 +1,6 @@
 if type -q ruby
   if not set -q GEM_HOME
     set -g GEM_HOME (ruby -e 'puts Gem.user_dir' 2> /dev/null)
-    and fish_add_path -g $GEM_HOME/bin
+    fish_add_path -g $GEM_HOME/bin
   end
 end
