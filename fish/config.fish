3ae0621fc24848f82be80eeca14a2e00e42fc907
Paul Kim <44695374+thekpaul@users.noreply.github.com>
2024-09-19 00:58:21 +0900

    FIX: Remove `conda`'s default Right-side Prompt
    Fish shell installed by `conda` have a specific snippet loaded after the
    user snippets specifically for the `conda` environment, in which is
    included a `__conda_add_prompt` function to add the right-side prompt.
    As this overwrites the user-defined `__conda_add_prompt` function
    intended to nullify it, this function is moved to the user's
    `config.fish` to ensure that it works the intended way.
    
    Signed-off-by: Paul Kim <44695374+thekpaul@users.noreply.github.com>

--- fish/config.fish
+++ fish/config.fish
@@ -2,6 +2,10 @@ set -gx GPG_TTY (tty)
 set -gx LC_CTYPE en_US.UTF-8
 set -gx LC_ALL en_US.UTF-8
 
+# NOTE: This needs to be sourced AFTER possible CONDA.fish snippet injection
+# kill the right prompt __conda_add_prompt 😠
+function __conda_add_prompt; end
+
 if [ (uname) = "Darwin" ]
   test -e {$HOME}/.iterm2_shell_integration.fish ;
     and source {$HOME}/.iterm2_shell_integration.fish
