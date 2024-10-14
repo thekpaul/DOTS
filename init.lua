fdd24e555a39ca29623d18070ed1df39b0637689
Paul Kim <44695374+thekpaul@users.noreply.github.com>
2024-10-14 17:10:50 +0900

    FEAT: Fallback for Erroneous Lazy.nvim Bootstrap
    With the addition of an error checker (`vim.v.shell_error`), Neovim now
    responds accordingly to an erroneous attempt to bootstrap Lazy.nvim on
    new or clean systems.
    
    Note that this patch still does not cover cases where the
    `vim.fn.system()` function itself is uncallable (e.g. on Neovim versions
    prior to its introduction) or unstable (e.g. on Windows systems).
    These cases may be covered in future commits.
    
    Signed-off-by: Paul Kim <44695374+thekpaul@users.noreply.github.com>

--- init.lua
+++ init.lua
@@ -185,13 +185,13 @@ if not vim.g.vscode then
 	-- }
 
 	-- Easy access to system clipboard
-  -- {
+	-- {
 	mapopts.desc = "Yank to System Clipboard"
 	map({ "n", "v" }, "<leader>y", "\"+y")
 
 	mapopts.desc = "Yank Current Line to System Clipboard"
 	map("n", "<leader>Y", "\"+Y")
-  -- }
+	-- }
 
 	-- Normal Mode for Terminal Buffer in Neovim
 	mapopts.desc = "Return to Normal Mode in Terminal Buffer"
@@ -232,14 +232,24 @@ if not vim.g.vscode then
 	-- Bootstrap LAZY.nvim as a native Lua plugin manager
 	local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 	if not (vim.uv or vim.loop).fs_stat(lazypath) then
-		vim.fn.system({
+		local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+		local out = vim.fn.system({
 			"git",
 			"clone",
 			"--filter=blob:none",
-			"https://github.com/folke/lazy.nvim.git",
+			lazyrepo,
 			"--branch=stable", -- latest stable release
-			lazypath,
+			lazypath
 		})
+		if vim.v.shell_error ~= 0 then
+			vim.api.nvim_echo({
+				{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+				{ out, "WarningMsg" },
+				{ "\nPress any key to exit..." },
+			}, true, {})
+			vim.fn.getchar()
+			os.exit(1)
+		end
 	end
 	vim.opt.rtp:prepend(lazypath)
 
