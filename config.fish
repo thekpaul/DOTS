f005afa49d3044c28c6d6a6e259c0da8fbcc3cae
Paul Kim <44695374+thekpaul@users.noreply.github.com>
2024-10-31 17:21:35 +0900

    FEAT: Cleanup Duplicate Items in `PATH` Vars
    This additional snippet removes duplicated items from the `PATH` and
    `LD_LIBRARY_PATH` environmental variables on session startup.
    
    Signed-off-by: Paul Kim <44695374+thekpaul@users.noreply.github.com>

--- config.fish
+++ config.fish
@@ -10,3 +10,32 @@ if [ (uname) = "Darwin" ]
   test -e {$HOME}/.iterm2_shell_integration.fish ;
     and source {$HOME}/.iterm2_shell_integration.fish
 end
+
+# Remove duplicate items from `PATH` and `LD_LIBRARY_PATH` (if any)
+set --path -l NEWPATH
+set --path -l PATHDUP
+for i in $PATH
+  if contains $i $NEWPATH
+    set -a PATHDUP $i
+  else if not test -z $i
+    set -a NEWPATH $i
+  end
+end
+# echo "Removing from path:" $PATHDUP
+# echo $NEWPATH
+set -g PATH $NEWPATH
+
+if set -q LD_LIBRARY_PATH
+  set --path -l NEWLIBS
+  set --path -l LIBSDUP
+  for i in $LD_LIBRARY_PATH
+    if contains $i $NEWLIBS
+      set -a LIBSDUP $i
+    else if not test -z $i
+      set -a NEWLIBS $i
+    end
+  end
+# echo "Removing from libs:" $LIBSDUP
+# echo $NEWLIBS
+  set -g LD_LIBRARY_PATH $NEWLIBS
+end
