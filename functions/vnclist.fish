ea1ab178ad4d7eccc8974c0c2d3236bcb12bbd87
Paul Kim <44695374+thekpaul@users.noreply.github.com>
2024-10-31 17:17:41 +0900

    FEAT: Args for Help and User-wise VNC Report
    This adds a preliminary help message (as `-h/--help`) and a user-wise
    reporting option (`-u/--user[=?]`) for the `vnclist` function.
    
    The user-wise reporting option (`-u/--user[=?]`) can receive up to one
    optional argument.
    If an optional argument is given, the function prints all ports used by
    the provided user.
    If an optional argument is NOT given (with only `-u/--user` as input),
    the function prints all currently inhabiting users and their respective
    ports.
    
    Output formats may change in future updates to this function.
    
    Signed-off-by: Paul Kim <44695374+thekpaul@users.noreply.github.com>

--- functions/vnclist.fish
+++ functions/vnclist.fish
@@ -1,4 +1,25 @@
 function vnclist
+  argparse 'h/help' 'u/user=?' -- $argv; or return
+  if set -q _flag_help
+    printf '%s\n' 'vnclist - display VNC usage on current host'
+    printf '%s\n' ''
+    printf '%s\n' 'vnclist'
+    printf '%s\n' 'vnclist -u[[=]USER]'
+    printf '%s\n' ''
+    printf '%s\n' 'DESCRIPTION'
+    printf '%s\n' '`vnclist` is a function to display current VNC server usage on the current host.'
+    printf '%s\n' 'It uses the `ps` executable to list all currently running processes,'
+    printf '%s\n' 'then parses the returned process descriptions to detect display numbers and their inhabiting users.'
+    printf '%s\n' ''
+    printf '%s\n' '· USER is the specific user to search for.'
+    printf '%s\n' ''
+    printf '%s\n' 'The following options are available:'
+    printf '%s\n' ''
+    printf '%s\n\t%s\n' '-h or --help' 'Displays help about using this command.'
+    printf '%s\n\t%s\n' '-u or --user' 'Searches and prints usage by user.'
+    return 0
+  end
+
   set -l VNCLIST
   for i in (ps -ef | grep vnc | awk ' { print $9 ":" $1 } ')
     if string match -r -- "^:.*" $i 2>&1 > /dev/null
@@ -25,5 +46,43 @@ function vnclist
       end
     end
   end
-  echo $VNCLIST
+
+  if set -q _flag_user
+    set -l VNCDICT
+    for i in $VNCLIST
+      set i (string split ':' $i)
+      if count $VNCDICT > /dev/null
+        for j in (seq 1 (count $VNCDICT))
+          if string match -qe -- $i[2] $VNCDICT[$j]
+            set VNCDICT[$j] (string join ' ' $VNCDICT[$j] $i[1])
+            break
+          end
+        end
+        if test $j = (count $VNCDICT)
+          set -a VNCDICT "$i[2] $i[1]"
+        end
+      else
+        set -a VNCDICT "$i[2] $i[1]"
+      end
+    end
+    if string length -q $_flag_user
+      if test (string sub -e 1 $_flag_user) = '='
+        set _flag_user (string sub -s 2 $_flag_user)
+      end
+      set PRINT_MATERIALS (string split ' ' (string match -e -- $_flag_user $VNCDICT))
+      echo "User $PRINT_MATERIALS[1] at port(s) $PRINT_MATERIALS[2..]"
+    else
+      echo "Print-by-user mode"
+      for i in (seq (count $VNCDICT))
+        printf '%s' $VNCDICT[$i]
+        if test $i = (count $VNCDICT)
+          printf '\n'
+        else
+          printf ' : '
+        end
+      end
+    end
+  else
+    echo $VNCLIST
+  end
 end
