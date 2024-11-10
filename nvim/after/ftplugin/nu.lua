310e903b052ebc3c2b0846fc7e33a83527e4e63f
Paul Kim <44695374+thekpaul@users.noreply.github.com>
2024-11-11 00:35:17 +0900

    FEAT(nvim): Nushell Language Filetype Config
    Adds new filetype config plugin for the Nushell language, which will be
    used in configuration files, functions and scripts used for Nushell.
    
    Signed-off-by: Paul Kim <44695374+thekpaul@users.noreply.github.com>

---
+++ nvim/after/ftplugin/nu.lua
@@ -0,0 +1,10 @@
+--[=[
+	INIT.lua: Initialising Configurations for NeoVim in Lua.
+	Ported and extended from original configurations in legacy vimscript.
+]=]
+
+-- Local Tab/Space Settings
+vim.opt_local.shiftwidth = 4
+vim.opt_local.tabstop = 4
+vim.opt_local.softtabstop = 4
+vim.opt_local.smarttab = true
