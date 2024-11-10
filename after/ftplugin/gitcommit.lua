b796606f066ac11ef1283ad3b60f7a036d2f2f87
Paul Kim <44695374+thekpaul@users.noreply.github.com>
2024-11-11 00:34:02 +0900

    STYLE: Edit Comments for Filetype Configs in Lua
    Fixes description for each filetype configuration plugin (Lua only) to
    correctly reflect their contents.
    
    Signed-off-by: Paul Kim <44695374+thekpaul@users.noreply.github.com>

--- after/ftplugin/gitcommit.lua
+++ after/ftplugin/gitcommit.lua
@@ -1,12 +1,12 @@
 --[=[
-	INIT.lua: Initialising Configurations for NeoVim in Lua.
-	Ported and extended from original configurations in legacy vimscript.
+	FTPLUGIN_GITCOMMIT.lua: Configurations for the Git Commit Message filetype.
 ]=]
 
--- Coloring columns
+-- Local Tab/Space Settings
 vim.opt_local.shiftwidth = 2
 vim.opt_local.tabstop = 2
 vim.opt_local.softtabstop = 2
 vim.opt_local.smarttab = true
 
+-- Coloring columns
 vim.opt.colorcolumn = "50,72"
