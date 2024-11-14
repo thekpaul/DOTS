e894a3a871f795765a063dd32ac9bfbd5b423d19
Paul Kim <44695374+thekpaul@users.noreply.github.com>
2024-11-15 01:51:39 +0900

    FIX: Suppress Filenames from `ic` Autocompletion
    The `ic` function is not meant to, and should not, accept filenames as
    its parameter.
    While previous autocompletion could not prevent such behaviour,
    when there were no viable `ic`-scripts found on the system,
    this commit suppresses filename autocompletions in such cases.
    
    Signed-off-by: Paul Kim <44695374+thekpaul@users.noreply.github.com>

--- completions/ic.fish
+++ completions/ic.fish
@@ -3,9 +3,14 @@ for command in (complete -C ic | grep command)
   set -a IC (string sub -s 3 (string match -r "ic\d+" $command))
 end
 
-for i in $IC
-  complete --command ic --no-files -n "not __fish_seen_subcommand_from $IC" \
-    -a $i -d "Apply `ic$i`"
+if count $IC > /dev/null
+  for i in $IC
+    complete --command ic --no-files -n "not __fish_seen_subcommand_from $IC" \
+      -a $i -d "Apply `ic$i`"
+  end
+else
+  complete --command ic --no-files
 end
+
 complete --command ic --no-files --short-option h --long-option help \
   --description "Print help"
