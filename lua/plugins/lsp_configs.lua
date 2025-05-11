337ca4bfb1f8a6f21329335401e1e57f6e8af981
Paul Kim <44695374+thekpaul@users.noreply.github.com>
2025-05-11 19:29:10 +0900

    FEAT: Update Deprecated `sign_define` to v0.11 API
    As `sign_define` functions are deprecated in Neovim v0.11,
    diagnostic sign definitions using this function are updated to use the
    newer API `vim.diagnostic.config`.
    Constituents `text` and `texthl`, as well as values assigned for each
    constituent, remain unchanged from previous implementations.
    
    Signed-off-by: Paul Kim <44695374+thekpaul@users.noreply.github.com>

--- lua/plugins/lsp_configs.lua
+++ lua/plugins/lsp_configs.lua
@@ -73,11 +73,22 @@ return {
 		vim.lsp.config('*', { capabilities = capabilities })
 
 		-- Diagnostic symbols in sign column (gutter)
-		local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
-		for type, icon in pairs(signs) do
-			local hl = "DiagnosticSign" .. type
-			vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
-		end
+		vim.diagnostic.config({
+			signs = {
+				text = {
+					[vim.diagnostic.severity.ERROR] = " ",
+					[vim.diagnostic.severity.WARN]  = " ",
+					[vim.diagnostic.severity.HINT]  = "󰠠 ",
+					[vim.diagnostic.severity.INFO]  = " "
+				},
+				texthl = {
+					[vim.diagnostic.severity.ERROR] = "DiagnosticSignError",
+					[vim.diagnostic.severity.WARN]  = "DiagnosticSignWarn",
+					[vim.diagnostic.severity.HINT]  = "DiagnosticSignHint",
+					[vim.diagnostic.severity.INFO]  = "DiagnosticSignInfo"
+				}
+			}
+		})
 
 		-- Lua LSP `lua_ls`: Managed via Mason.nvim
 		vim.lsp.config('lua_ls', {
